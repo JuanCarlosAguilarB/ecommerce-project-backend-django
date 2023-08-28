0e1ab3f8f145ee5ed474f2e9ae269ebdcab3b0a2
Juan Carlos A <abjuancarlos.12@gmail.com>
2023-08-27 22:16:40 -0500

    fix

--- render/build.sh
+++ render/build.sh
@@ -8,12 +8,12 @@ pip install -r ./requirements/production.txt
 # python manage.py collectstatic --no-input
 
 echo "---apply database migrations---"
-python manage.py makemigrations --settings=core.settings.local
+# python manage.py makemigrations --settings=core.settings.local
 python manage.py migrate --settings=core.settings.local
 
 # create superuser by default
 echo "---create superuser---"
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin@example.com', 'Admin12345678#')" | python manage.py shell --settings=core.settings.local
+# echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin@example.com', 'Admin12345678#')" | python manage.py shell --settings=core.settings.local
 
 # runserver
 echo "---run server---"
