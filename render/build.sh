920706bbc5779c64e0e8b76ba53178329b8268d1
Juan Carlos A <abjuancarlos.12@gmail.com>
2023-11-19 14:29:33 -0500

    fix

--- render/build.sh
+++ render/build.sh
@@ -3,6 +3,24 @@
 # Exit on error
 set -o errexit
 
+pip install -r ./requirements/production.txt
+
+# python manage.py collectstatic --no-input
+
+echo "---apply database migrations---"
+# python manage.py makemigrations --settings=core.settings.local
+python manage.py migrate --settings=core.settings.local
+
+# create superuser by default
+echo "---create superuser---"
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin@gmail.com', 'Admin12345678#')" | python manage.py shell --settings=core.settings.local
+
+# runserver
+echo "---run server---"
+# python manage.py runserver --settings=core.settings.local 0.0.0.0:8000
+# gunicorn core.wsgi
+
+
 # Check for Redis existence
 if ! command -v redis-server &> /dev/null
 then
@@ -22,19 +40,3 @@ fi
 echo "Starting Redis server..."
 redis-server &
 
-pip install -r ./requirements/production.txt
-
-# python manage.py collectstatic --no-input
-
-echo "---apply database migrations---"
-# python manage.py makemigrations --settings=core.settings.local
-python manage.py migrate --settings=core.settings.local
-
-# create superuser by default
-echo "---create superuser---"
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin@gmail.com', 'Admin12345678#')" | python manage.py shell --settings=core.settings.local
-
-# runserver
-echo "---run server---"
-# python manage.py runserver --settings=core.settings.local 0.0.0.0:8000
-# gunicorn core.wsgi
