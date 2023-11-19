5f707e31842c1687ea0105f163e8532a33b57eee
Juan Carlos A <abjuancarlos.12@gmail.com>
2023-11-19 15:23:34 -0500

    fix

--- render/build.sh
+++ render/build.sh
@@ -43,13 +43,13 @@
 
 
 #!/bin/bash
-sudo apt install lsb-release curl gpg
+apt install lsb-release curl gpg
 
-curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
+curl -fsSL https://packages.redis.io/gpg | gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
 
-echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
+echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/redis.list
 
-sudo apt-get update
-sudo apt-get install redis
+apt-get update
+apt-get install redis
 
-sudo snap install redis
+snap install redis
